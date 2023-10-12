e654b604df135f7bf68eb4b6d1a48c64497bbcda
Rostamba <147610959+Rostamba1@users.noreply.github.com>
2023-10-12 15:56:06 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,17 +2,22 @@ def solve_quadratic(a, b, c):
     d = b * b - 4 * a * c # Discriminant of quadratic equation
     if d < 0:
         return []
+    elif d == 0:
+        x = -b / (2 * a)
+        return [x]
     else:
-        if d == 0:
-            x = -b / (2 * a)
-            return [x]
-        else:
-            x_1 = (-b + d ** 0.5) / (2 * a)
-            x_2 = (-b - d ** 0.5) / (2 * a)
-            return [x_1, x_2]
+        x_1 = (-b + d ** 0.5) / (2 * a)
+        x_2 = (-b - d ** 0.5) / (2 * a)
+        return [x_1, x_2]
 
-print("Определение корней уравнения ax² + bx + c = 0")
-a = float(input("Введите число a >>> "))
-b = float(input("Введите число b >>> "))
-c = float(input("Введите число c >>> "))
-print(solve_quadratic(a, b, c))
+print('Определение корней уравнения ax² + bx + c = 0')
+a = float(input('Введите число a >>> '))
+b = float(input('Введите число b >>> '))
+c = float(input('Введите число c >>> '))
+x = solve_quadratic(a, b, c)
+if len(x) == 0:
+    print('Нет действительных корней')
+elif len(x) == 1:
+    print('x =', x[0])
+else:
+    print('x_1 = ', x[0], ', x_2 =', x[1], sep='')
